5d275a2c4b6d94ee2a09ad1f6e724dd41eee15e1
David Altenburg <daltenburg@drwholdings.com>
2014-12-16 20:46:11 -0600

    Added alphabet cipher solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,35 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet (seq "abcdefghijklmnopqrstuvwxyz"))
+
+
+ (def encode-table
+      (reduce merge
+              (map-indexed (fn [i row] {row
+                                        (reduce merge
+                                                (map
+                                                 (fn [col val] {col val})
+                                                 alphabet
+                                                 (concat (drop i alphabet) (take i alphabet))))}) alphabet)))
+(def decode-table
+  (reduce merge
+          (map-indexed (fn [i row] {row
+                                    (reduce merge
+                                            (map
+                                             (fn [col val] {col val})
+                                             alphabet
+                                             (concat  (drop (- 26 i) alphabet) (take (- 26  i) alphabet))))}) alphabet)))
+
+
+(defn- make-key [keyword len]
+  (take len (apply concat (repeat keyword))))
+
+(defn- apply-table [table keyword message]
+  (let [key (make-key keyword (count message))]
+    (apply str (map (fn [src key] (get-in table [key src])) message key))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply-table encode-table keyword message))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply-table decode-table keyword message))
