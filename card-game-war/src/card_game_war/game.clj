70fb47fa4ffdadbd7ff61f00d0aa60f098c96011
David Altenburg <daltenburg@drwholdings.com>
2014-12-23 20:12:04 -0600

    Added solution to war card game

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -2,12 +2,34 @@
 
 ;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
-(def ranks [2 3 4 5 6 7 8 9 :jack :queen :king :ace])
+(def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
 (def cards
   (for [suit suits
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn keyfn [[suit rank]]
+  (+
+   (* 100 (.indexOf ranks rank))
+   (.indexOf suits suit)))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-round [player1-card player2-card]
+  (if (> (keyfn player1-card) (keyfn player2-card))
+    [[player1-card player2-card] []]
+    [[] [player2-card player1-card]]))
+
+(defn next-step [[player1-cards player2-cards]]
+  (if (or (empty? player1-cards) (empty? player2-cards))
+    nil
+    (let [[player1-result player2-result]
+          (play-round (first player1-cards) (first player2-cards))]
+      [(concat (rest player1-cards) player1-result)
+       (concat (rest player2-cards) player2-result)])))
+
+(defn play-game [player1-cards player2-cards]
+  (let [game-steps (take-while identity (iterate next-step [player1-cards player2-cards]))
+        end-state (last game-steps)
+        [player1-end player2-end] end-state]
+    (cond (not (empty? player1-end)) :player1
+          (not (empty? player2-end)) :player2
+          :else :draw)))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,19 +2,40 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higer rank than jacks")
-  (testing "kings are higer rank than queens")
-  (testing "aces are higer rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= [[[:spade 4] [:spade 2]] []] (play-round [:spade 4] [:spade 2])))
+    (is (= [[] [[:spade 4] [:spade 2]]] (play-round [:spade 2] [:spade 4]))))
+  (testing "queens are higer rank than jacks"
+    (is (= [[[:spade :queen] [:spade :jack]] []] (play-round [:spade :queen] [:spade :jack]))))
+  (testing "kings are higer rank than queens"
+    (is (= [[[:spade :king] [:spade :queen]] []] (play-round [:spade :king] [:spade :queen]))))
+  (testing "aces are higer rank than kings"
+    (is (= [[[:spade :ace] [:spade :king]] []] (play-round [:spade :ace] [:spade :king]))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= [[[:club 2] [:spade 2]] []] (play-round [:club 2] [:spade 2]))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= [[[:diamond 2] [:club 2]] []] (play-round [:diamond 2] [:club 2]))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= [[[:heart 2] [:diamond 2]] []] (play-round [:heart 2] [:diamond 2])))))
+
+(deftest test-game-state
+  (testing "the winner of the round gets those cards at the bottom of their deck"
+    (let [player1-start [[:spade :queen] [:heart 2]]
+          player2-start [[:spade :jack] [:heart 3]]]
+
+      (is (= [[[:heart 2] [:spade :queen] [:spade :jack]]
+              [[:heart 3]]]
+             (next-step [player1-start player2-start])))))
+  (testing "when a player is out of cards, the next game state is nil"
+    (let [player1-start [[:heart 2]]
+          player2-start []]
+      (is (= nil (next-step [player1-start player2-start])))
+      (is (= nil (next-step [player2-start player1-start]))))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards")
-  (testing "if both players run out of cards then it is a draw"))
-
+  (testing "the player loses when they run out of cards"
+      (let [win-start [[:heart 10]]
+            lose-start [[:heart 9]]]
+        (is (= :player1 (play-game win-start lose-start)))
+        (is (= :player2 (play-game lose-start win-start))))))
